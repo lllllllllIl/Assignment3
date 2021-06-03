3eb871bd350dc2ef15b9d3a0adc4004c7435c387
Robert Apacible <gorobbie@gmail.com>
2021-06-03 13:55:40 +1000

    initial commit

---
+++ website/models.py
@@ -0,0 +1,65 @@
+### this would go into a models.py or something similar ###
+from . import db
+#We use ev_ent because event is a used term
+from datetime import datetime
+from sqlalchemy.types import Boolean
+
+class User(db.Model):
+    __tablename__='users' # good practice to specify table name
+    
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    emailid = db.Column(db.String(100), index=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    admin = db. Column(db.Boolean)
+
+class events (db.Model):
+      __tablename__= 'Events'
+
+      __table_args__ = {'extend_existing': True}
+
+      id = db.Column(db.Integer, primary_key=True)
+      name = db.Column(db.String(80))
+      description = db.Column(db.String(200))
+      image = db.Column(db.String(400))
+      date = db.Column(db.DateTime)
+def __repr__(self): 
+    return "<Name: {}>".format(self.name)
+
+class booking (db.Model):
+   __tablename__='Bookings'
+
+   __table_args__ = {'extend_existing': True}
+
+   event_id = db.Column(db.Integer, db.ForeignKey('Events.id') )
+   price = db.Column(db.Integer)
+   qty = db.Column(db.Integer)
+   date = db.Column(db.DATE)
+   bookid = db.Column(db.Integer, primary_key = True)
+   user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class comments (db.Model):
+    __tablename__ = 'comments'
+
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(400))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    event_id = db.Column(db.Integer,db.ForeignKey('Events.id'))
+def __repr__(self):
+    return "<Comment: {}>".format(self.text)
+
+
+# class created (db.Model):
+#      __tablename__ = 'Creted events'
+
+#      __table_args__ = {'extend_existing': True}
+
+#      event_id = db.Column(db.Integer, db.ForeignKey('Events.id'))
+#      creator = db.Column(db.String)
+#      Date_Created= db.Column(db.DateTime, default=datetime.now())
--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from .models import events
+
 
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    return render_template('index.html', events = events)
