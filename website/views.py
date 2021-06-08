0577bd82d566127dfa0cd997bea1e34c4884667d
Robert Apacible <gorobbie@gmail.com>
2021-06-08 11:16:41 +1000

    initial commit

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, session, redirect, url_for
 from .models import events
 
 
@@ -10,3 +10,31 @@ def index():
     Event = events.query.all()
 
     return render_template('index.html', Event = events)
+
+@bp.route('/EventCreation')
+def eventCreation():
+
+    return render_template('eventCration.html')
+
+@bp.route('/EventDetails')
+def eventDetails():
+    return render_template('eventDetails.html')
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    #if 'email' in session:
+    #   return redirect('/')
+    
+    email = request.values.get('email')
+    pwd = request.values.get('pwd')
+    phone = request.values.get('phone')
+
+    session['email'] = email
+
+    return render_template('login.html')
+
+@bp.route('/logout')
+def logout():
+    if 'email' in session:
+        session.pop('email', None)
+    return 'Session has been cleared'
